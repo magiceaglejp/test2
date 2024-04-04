94a1f6294390bfac5920bef1eb923eefac15d308
magiceaglejp <165649871+magiceaglejp@users.noreply.github.com>
2024-04-04 11:51:23 +0900

    test.js の更新

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var button1 = 0;
-document.getElementById("texttest") = 0
+document.getElementById("texttest") value = 0
 function button(){
   button1 = button1 + 1;
   document.getElementById("texttest").innerHTML = button1;
